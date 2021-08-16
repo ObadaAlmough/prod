089ab098da0a77dbd2dd7fafdb8531ba35d712f1
obadaAlmoug <83464909+obadaAlmoug@users.noreply.github.com>
2021-08-16 23:32:57 +0200

    new

--- proda.php
+++ proda.php
@@ -19,8 +19,9 @@ $newArray = $_SESSION['prodacts'];
 
 
 <head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/all.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
@@ -32,6 +33,7 @@ $newArray = $_SESSION['prodacts'];
 
             <?php
 
+    
 
 
 
@@ -81,8 +83,9 @@ $newArray = $_SESSION['prodacts'];
 
 
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- prodacts.php
+++ prodacts.php
@@ -6,16 +6,8 @@ if(isset($_GET)){
 session_start();
 
 
-$prodacts = [
-    0 => ['image'=>'https://imgur.com/edOjtEC.png' , 'price' => '22,495' , 'fuel' => '8.5/7.1'],
-    1 => ['image'=> 'https://imgur.com/gUQNKPd.png', 'price' => '29,195' , 'fuel' => '8.4/2.1'],
-    2 => ['image'=>'https://imgur.com/edOjtEC.png' , 'price' => '22,495' , 'fuel' => '8.5/7.1'],
-    3 =>['image'=> 'https://imgur.com/gUQNKPd.png', 'price' => '29,195' , 'fuel' => '8.4/2.1'],
-    4 => ['image'=> 'https://imgur.com/SKZolYE.png', 'price' =>'21,935' , 'fuel' => '8.2/6.1'],
-    5 => ['image'=> 'https://imgur.com/SKZolYE.png', 'price' =>'21,935' , 'fuel' => '8.2/6.1'],
 
-
-] ;
+$prodacts = json_decode($_SESSION['arrProdact'],true);
 
 
 if($_SESSION['prodacts'] != null){
